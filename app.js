5cc226847ea9a2071bf73c36e0ce09ddf68a5f78
Moumita Saha <sahamoumita318@gmail.com>
2019-02-27 21:28:47 +0530

    project completed

--- app.js
+++ app.js
@@ -8,3 +8,87 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores,roundScore,activePlayer,gamePlaying;
+
+init();
+
+
+
+document.getElementById("roll-dice").addEventListener("click", function(){
+ if(gamePlaying){
+  
+  
+  
+var randomNumber=Math.floor(Math.random()*6)+ 1;
+document.getElementById("current-" + activePlayer).textContent = randomNumber;
+
+
+  var diceDom=document.querySelector('.dice');
+  diceDom.style.display='block';
+  diceDom.src="dice-"+ randomNumber + ".png";
+  
+  if(randomNumber!==1){
+    
+    roundScore += randomNumber;
+ document.getElementById("current-" + activePlayer).textContent =roundScore;   
+    
+  }else{
+    nextPlayer();
+     
+  }
+ }
+});
+  
+  document.querySelector(".btn-hold").addEventListener("click", function(){
+    if (gamePlaying){
+    scores[activePlayer] += roundScore;
+    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    
+    if(scores[activePlayer] >= 100){
+        document.querySelector("#name-" + activePlayer).textContent ="Winner";
+      document.querySelector('.dice').style.display="none";
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+          document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying=false;
+   }
+  else{
+nextPlayer();
+  }
+    }
+    
+  });
+
+document.getElementById("new-game").addEventListener("click", init);
+  
+function nextPlayer(){
+   document.getElementById("current-" + activePlayer).textContent ="0";
+    activePlayer===0? activePlayer = 1:activePlayer =0;
+    roundScore=0;
+  
+  document.getElementById("current-1").textContent ="0";
+  document.getElementById("current-0").textContent ="0";
+    
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  document.querySelector(".dice").style.display="none";
+}
+
+function init(){
+ scores=[0,0];
+roundScore=0;
+activePlayer=0;
+ gamePlaying=true;
+document.querySelector(".dice").style.display="none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+document.getElementById("name-0").textContent = "Player 1";
+document.getElementById("name-1").textContent = "Player 2";
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+}
